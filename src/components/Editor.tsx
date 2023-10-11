68ab2ebf0c5a150c80294a037cfd84d40b15f5bb
ezeugo <ezeugo@copilot.com>
2023-10-11 12:41:04 +0100

    got the editor working

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -18,7 +18,31 @@ export function Editor(props: Props) {
       <button
         style={{ display: "block" }}
         onClick={() => {
-          localStorage.setItem(clientId, text);
+          const currentValue = localStorage.getItem(clientId);
+
+          // Create a new Date object to get the current date and time
+          const currentDate = new Date();
+
+          // Get the current time in various formats
+          const currentHour = currentDate.getHours(); // Get the current hour (0-23)
+          const currentMinute = currentDate.getMinutes(); // Get the current minute (0-59)
+          const currentSecond = currentDate.getSeconds(); // Get the current second (0-59)
+
+          const timeStamp = `${currentHour}:${currentMinute}:${currentSecond}`;
+
+          if (currentValue === undefined || currentValue === null) {
+            localStorage.setItem(
+              clientId,
+              JSON.stringify({
+                [timeStamp]: text,
+              })
+            );
+            location.reload();
+            return;
+          }
+          const currentMap = JSON.parse(currentValue);
+          currentMap[timeStamp] = text;
+          localStorage.setItem(clientId, JSON.stringify(currentMap));
           location.reload();
         }}
       >
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,10 +9,20 @@ interface Props {
 export function Sidebar(props: Props) {
   const { selectedUserId } = props;
   const [lastChangedValue, setLastChangedValue] = useState("");
+  const notesMap = JSON.parse(localStorage.getItem(selectedUserId) || "{}");
   return (
     <>
       Notes
-      <div>{localStorage.getItem(selectedUserId)}</div>
+      <div>
+        {Object.entries(notesMap).map(([key, value]) => {
+          return (
+            <div key={key}>
+              <span>{key}</span>
+              <span style={{ marginLeft: "20px" }}>{value}</span>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
