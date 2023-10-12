bb99d552d8f59ac80f9ac867b9d219a849a1792c
Brian Wilson <wilson.brian90@gmail.com>
2023-10-12 14:06:59 +0300

    increase note stack size and put horizontal titles on collapsed pages

--- src/components/NotePage.tsx
+++ src/components/NotePage.tsx
@@ -4,17 +4,33 @@ import { valueParser } from "./Sidebar";
 
 interface Props {
   noteId: string;
+  isHidden: boolean;
 }
 
 export function NotePage(props: Props) {
-  const { noteId } = props;
+  const { noteId, isHidden } = props;
   const store = JSON.parse(localStorage.getItem("notes") || "{}");
   const note = store[noteId];
 
   return (
     <div>
-      <h1>{note.title}</h1>
-      <p style={{width: "600px"}}>{valueParser(note.content)}</p>
+      <div>
+        {isHidden ? 
+        <h1 style={{
+          transform: "rotate(180deg)",
+          textOrientation: "sideways",
+          writingMode: "vertical-rl",
+          display: "flex",
+        }}>{note.title}</h1>
+         : 
+         <div>
+        <h1>{note.title}</h1>
+        <p style={{width: "600px"}}>{valueParser(note.content)}</p>
+        </div>
+
+      }
+      </div>
+    
     </div>
   );
 }
